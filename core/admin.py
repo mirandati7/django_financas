324d37107c6e04b69bae34bdde43f16c2f8c973c
Alex Miranda <you@example.com>
2024-08-30 21:44:08 -0300

    Projeto Finanças Admin

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
+from django.contrib.admin import AdminSite
 
-from .models import Categoria, Conta
+from .models import Categoria, Conta, LancamentoFinanceiro
 
 # Register your models here.
 
+admin.site.site_header = "Projeto Finanças"
+admin.site.site_title  = "Projeto Finanças"
+admin.site.index_title = "Bem-vindo ao Projeto Finanças"
+
+class LancamentoAdmin(admin.ModelAdmin):
+    list_display = ('data', 'historico','credito_debito','valor')
+
 admin.site.register(Categoria)
 admin.site.register(Conta)
+admin.site.register(LancamentoFinanceiro, LancamentoAdmin)
+
--- core/models.py
+++ core/models.py
@@ -12,3 +12,32 @@ class Conta(models.Model):
 
     def __str__(self):
         return self.nome
+    
+ATIVO_INATIVO = (
+    ('A', 'Ativo'),
+    ('I', 'Inativo')
+)   
+
+CREDITO_DEBITO = (
+    ('C', 'Crédito'),
+    ('D', 'Débito')
+)
+
+
+class LancamentoFinanceiro(models.Model):
+    valor = models.DecimalField(max_digits=9, decimal_places=2, default=0)
+    data = models.DateField()
+    # C = Credito   | D = Debito
+    credito_debito = models.CharField(("Operação"),max_length=1,choices=CREDITO_DEBITO, default='C')
+    historico = models.CharField(max_length=100, default=None)
+    # A = Ativo  | I - Inativo
+    ativo_inativo = models.CharField(max_length=1,choices=ATIVO_INATIVO, default='A')
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    conta = models.ForeignKey(Conta, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.historico 
+
+
+
+
